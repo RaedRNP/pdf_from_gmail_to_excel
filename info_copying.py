131aaa28614af45a10ce6044bb8e50d361bdca57
Raed <raedrnp10@gmail.com>
2023-10-14 12:53:01 -0400

    Fix: (info_copying) regex in sub_total changed and added avena and pila to s

--- info_copying.py
+++ info_copying.py
@@ -3,6 +3,7 @@ import os
 import re
 import pandas as pd
 
+os.chdir('./Download')
 
 def organizer():
 
@@ -17,12 +18,22 @@ def organizer():
         cliente = re.search(r"Cliente:\s+(.+)\s", text).group().split("\n")[0].split(":  ")[1].split(" (")[0]
         fecha = re.search(r"Factura:\s+(.+)\s", text).group().split("\n")[1]
         nota = re.search(r"Vendedor:\s+(.+)\s", text).group().split("\n")[1].replace(" ", "")
-        sub_total = re.search(r"I.V.A.:\s+(.+)\s", text).group().split("\n")[1].replace(" ", "")
+        sub_total = re.search(r"..........\s.\sI.V.A.:", text).group().split("\n")[0].replace(" ", "")
         
         preparado = re.search(r"PREPARADO", text)
+        avena = re.search(r"AVENA", text)
+        pila = re.search(r"PILA", text)
         
         if preparado is not None:
             s = [f"{cliente} (PREPARADO)", f"FACT- {nota}", fecha, sub_total]
+            sdf.append(s)
+            continue
+        if avena is not None:
+            s = [f"{cliente} (AVENA)", f"FACT- {nota}", fecha, sub_total]
+            sdf.append(s)
+            continue
+        if pila is not None:
+            s = [f"{cliente} (PILA)", f"FACT- {nota}", fecha, sub_total]
             sdf.append(s)
             continue
         
