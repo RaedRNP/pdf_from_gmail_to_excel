66fd0fd14bece0d6aa01557881ea7e95d58660e2
Raed <raedrnp10@gmail.com>
2023-10-19 11:46:07 -0400

    Fixed: deleting script

--- deleting.py
+++ deleting.py
@@ -3,8 +3,8 @@ import os
 os.chdir('./Download')
 
 def deleting():
-    if os.listdir('./Download') is not []:
-        files = [f for f in os.listdir('./Download')
+    if os.listdir('.') is not []:
+        files = [f for f in os.listdir('.')
                  if os.path.isfile(f)]
         for f in files:
             if "PROFORMA" in f:
@@ -12,3 +12,4 @@ def deleting():
             
     print("Done")
     
+deleting()
